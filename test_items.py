c33a07838521fc373824f66157c20010127177c7
Aleks4512 <98872329+Aleks4512@users.noreply.github.com>
2022-03-03 18:54:34 +0300

    Update test_items.py
    for comfortable add import time

--- test_items.py
+++ test_items.py
@@ -1,10 +1,13 @@
 from idlelib import browser
 from selenium.webdriver.common.by import By
+import time
 
 class TestBasket:
     def test_checked_button_add_baskets(self, browser):
         browser.implicitly_wait(2)
         link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
         browser.get(link)
+        # place for command "time.sleep(30)"
+        
         list_of_names_buttons = browser.find_elements(By.CSS_SELECTOR, ".btn-add-to-basket")
         assert len(list_of_names_buttons), "button 'Add to basket' not found on the page !"
