89060aa447629e94095c6d1c8c20a4904463a145
Alex4512 <alexandrovich601@mail.ru>
2022-03-03 18:32:07 +0300

    add space for def

--- test_items.py
+++ test_items.py
@@ -1,8 +1,10 @@
+from idlelib import browser
+
 
 class TestBasket:
     def test_checked_button_add_baskets(self, browser):
-    browser.implicitly_wait(2)
-    link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
-    browser.get(link)
-    list_of_names_buttons = browser.find_elements(By.CSS_SELECTOR, ".btn-add-to-basket")
-    assert len(list_of_names_buttons), "button 'Add to basket' not found on the page !"
+        browser.implicitly_wait(2)
+        link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
+        browser.get(link)
+        list_of_names_buttons = browser.find_elements(By.CSS_SELECTOR, ".btn-add-to-basket")
+        assert len(list_of_names_buttons), "button 'Add to basket' not found on the page !"
